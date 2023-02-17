3c31467dc8efc8ac7cfe8adaa232c7dc7ce13bdd
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-16 16:10:43 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Use puts to print a line of code
+ *
+ * Return: 0
+*\
+int made(void)
+{
+	puts("\"includeProgramming is like building a multilingual puzzle");
+	return (0);
+}
