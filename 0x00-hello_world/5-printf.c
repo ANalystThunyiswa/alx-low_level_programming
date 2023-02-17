4239692e6491e1800ab56cb5ceee24bafcba0d5c
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-16 16:41:08 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Use the function printf to print
+ * Return: (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	Return (0);
