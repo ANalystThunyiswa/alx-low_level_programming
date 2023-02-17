f2f007de86d11fd707a3a8d68e3d2c96ce0365b6
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-17 01:31:54 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A program that does not use any function listed in the NAME section of man printf or man puts
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
