001a55e4fe893ee3ca38e66c6f96e1ca6c7f299a
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-17 00:57:47 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main = A program that does not use any function listed in the NAME section of man printf or man puts
+ * Return: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
