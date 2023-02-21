299bbf0bb52fc3caca97fa203328f5ea1ecc8ad8
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-21 09:42:14 -0800

    My header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
 
-/**
- * main - A program that prints _putchar.
- *
- * Return: 0 (Success)
- *
- */
 
-int main(void)
-{
-	printf("_putchar\n");
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
+	}
 
-}
