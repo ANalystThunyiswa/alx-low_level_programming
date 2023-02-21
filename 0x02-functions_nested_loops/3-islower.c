2b3a67a45dd59c16ac394a9c15c08065b028528f
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-21 14:22:41 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,9 +6,14 @@
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	char i;
+	int lower = 0;
 
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+
+	return (lower);
 }
