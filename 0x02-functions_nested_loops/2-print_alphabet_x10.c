cba9fe58b2dc3b644bfec5dfe69e0bf9a0d44be8
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-21 13:58:44 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  * print_alphabet_x10 - Prints the print_alphabet 10 times.
+  * Return: 0 (Success)
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 1; i <= 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
