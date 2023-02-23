8a83bed400843fed556b9448d6151f1d3dccaf7a
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-23 02:54:53 -0800

    Causes an infinite loop

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+    int a = 972;
+    int b = -98;
+    int c = 0;
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+    (void)a
+    (void)b
+    (void)c
+    return (largest);
+}
