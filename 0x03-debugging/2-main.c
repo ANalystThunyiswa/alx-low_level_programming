b9552e804dd6d9d3a3ba422e74befc269925bd57
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-23 05:17:25 -0800

    Largest number

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,9 +11,9 @@ int main(void)
         int a, b, c;
         int largest;
 
-        a = 972;
+        a = 0;
         b = -98;
-        c = 0;
+        c = 972;
 
         largest = largest_number(a, b, c);
 
