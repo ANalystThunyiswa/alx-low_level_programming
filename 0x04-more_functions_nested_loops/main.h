41f3459189139776de6cdda58b3b0aaaf6b58bc6
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-23 11:42:53 -0800

    More numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,9 +3,10 @@
 
 #include <stdio.h>
 
-int _putchar(char c)
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif /* MAIN_H */
