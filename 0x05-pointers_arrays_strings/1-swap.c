55ab94000f07698637e022145d24c5ac4e92a3f1
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-02-27 06:03:04 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap - swap value of a with value of b.
+ * @a: input swap with b
+ * @b: input swap with a
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
