605e386b384b8abf5ddc7fa5f81baccd91233364
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-03-01 15:48:34 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,20 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
-	for (j = 0; dest[j] != '\0'; j++)
-		;
-	while (src[i] != '\0')
+
+	while (dest[i] != '\0')
 	{
-		dest[j + i] = src[i];
 		i++;
 	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
+
