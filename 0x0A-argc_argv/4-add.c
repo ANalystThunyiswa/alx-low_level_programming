7bfc7ad808ded257f0434e54b95112a8880bd60e
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-03-13 13:25:57 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>	
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 /**
@@ -26,7 +26,7 @@ int check_num(char *str)
 
 
 		count++;
-	}	
+	}
 		return (1);
 }
 
@@ -45,40 +45,39 @@ int main(int argc, char *argv[])
 
 {
 
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
+
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
+	{
+		if (check_num(argv[count]))
 
 
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
 		{
-			if (check_num(argv[count]))
-
-
-			{
-				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-				sum += str_to_int;
-			}
-
-
-			/*Condition if one of the number contains symbols that are not digits*/
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-
-
-			count++;
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
+
+
+	/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+
+		count++;
+	}
 
 
 		printf("%d\n", sum); /*print sum*/
 
 
 		return (0);
-	}
+}
 
