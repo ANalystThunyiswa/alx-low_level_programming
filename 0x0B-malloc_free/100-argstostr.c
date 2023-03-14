3d57d9b81cb1e715827ef53ebd7cd2210285718c
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-03-14 15:26:50 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,6 @@
-
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
  * argstostr - concatenates all arguments of program.
  * @ac: argument count
@@ -9,43 +9,30 @@
  */
 char *argstostr(int ac, char **av)
 {
+	int i, j, len, bufferlen;
+	char *p;
 
-	int i, n, r = 0, l = 0;
-
-	char *str;
-
-	if (ac == 0 || av == NULL)
-
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
 		return (NULL);
 
-	for (i = 0; i < ac; i++)
+	i = j = len = bufferlen = 0;
+	for (i = 0; av[i]; i++)
 	{
-
-		for (n = 0; av[i][n]; n++)
-
-			l++;
-
+		for (j = 0; av[i][j]; j++)
+			len++;
 	}
-
-	l += ac;
-
-	str = malloc(sizeof(char) * l + 1);
-
-	if (str == NULL)
-
+	p = (char *)malloc(len * sizeof(char) + ac + 1);
+	if (p == NULL)
 		return (NULL);
-
-	for (i = 0; i < ac; i++)
+	for (i = 0; av[i]; i++)
 	{
-	for (n = 0; av[i][n]; n++)
-	{
-		str[r] = av[i][n];
-
-		r++;
+		for (j = 0; av[i][j]; j++, bufferlen++)
+			p[bufferlen] = av[i][j];
+		p[bufferlen] = '\n';
+		bufferlen++;
 	}
-	if (str[r] == '\0')
-	{
-		str[r++] = '\n';
-
-	}
+	p[bufferlen] = '\0';
+	return (p);
 }
