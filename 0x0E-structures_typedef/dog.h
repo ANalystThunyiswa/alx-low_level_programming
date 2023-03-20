e3ba17d0563d7078dd1d9a09356ca92e8142f254
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-03-20 15:59:16 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,12 @@
 #define DOG_H
 
 /**
- * Description: Defines metadata about the dog and type of dog's struct
- *
  * struct dog - dog data
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner of the dog
  *
+ * Desription: Defines the dog and type of do's struct
  */
 typedef struct dog
 {
