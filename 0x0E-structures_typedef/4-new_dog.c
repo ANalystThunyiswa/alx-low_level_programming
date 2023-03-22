0cb6b9c1c3966c644c2410c8b28ac65519bc5c3d
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-03-21 22:17:33 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,6 +43,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
 /**
@@ -73,6 +74,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
+
 	if (dog->owner == NULL)
 	{
 		free(dog);
