a45eff3fcedaedc7da3794f04f1110b3b7d39f4a
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-03-22 23:28:16 -0700

    calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -13,13 +13,13 @@ typedef struct op
     int (*f)(int a, int b);
 } op_t;
 
-/*other prototypes used by the program*/ 
+/*Prototypes used by the program*/ 
 
-int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif /* CALC_H*/
