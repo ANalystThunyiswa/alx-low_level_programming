533301e955db3e03420da56d4a43d2ebcd833d35
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-04-03 21:31:41 -0700

    updated print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,8 +3,8 @@
 /**
  * print_listint - prints all elements of the list
  *
- * @h: pointer to the head node of the list
- * Return: the number of nodes
+ * @h: pointer to the list to print
+ * Return: the number of nodes in the list
  */
 
 size_t print_listint(const listint_t *h)
