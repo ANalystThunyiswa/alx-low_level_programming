7638ba9c41f793ebbc79c829f669e70842f40cd6
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-04-14 05:24:40 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	my_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (my_file == -1)
+	if (my_file < 0)
 	{
 		return (-1);
 	}
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	write_status = write(my_file, text_content, len);
 	close(my_file);
 
-	if (write_status == -1)
+	if (write_status < 0)
 	{
 		return (-1);
 	}
