f27cf5059eea1583533cd4ddd690cb88c318b0b7
ANalystThunyiswa <hlubithunyiswa@gmail.com>
2023-04-14 00:28:04 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,17 +22,21 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-
-	while (text_content != NULL)
+	if (text_content == NULL)
 	{
-		for (len = 0; text_content[len]; len++)
-			;
-		write_status = write(my_file, text_content, len);
-		close(my_file);
+		return (1);
 	}
+	for (len = 0; text_content[len]; len++)
+			;
+	write_status = write(my_file, text_content, len);
+	close(my_file);
+
 	if (write_status == -1)
 	{
 		return (-1);
 	}
-	return (1);
+	else
+	{
+		return (1);
+	}
 }
